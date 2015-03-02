3dba3eaa9a641c97d32a3405017289624ce395d1
Jamie Chung <me@jamiechung.me>
2015-03-02 11:12:48 -0800

    Mimic styles from help popup + expando logic

---
+++ js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+
+    $.fn.extend({
+        ExpanderList: function (options) {
+            var el = $(this);
+            el.addClass('expando-list');
+            el.children().each(function () {
+                var child = $(this);
+                child.Expander(options);
+            });
+        },
+
+        Expander: function (options) {
+            var expando = $(this).addClass('expando');
+
+            var link = expando.children().first().addClass('expando-head');
+            var content = link.next().addClass('expando-body').hide();
+
+            link.click(function(e){
+                e.preventDefault();
+                content.slideToggle();
+            });
+
+        }
+    });
+
+})(jQuery);
